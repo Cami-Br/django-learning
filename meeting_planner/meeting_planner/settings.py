2f348d4146ce8ff31e3514d24068d41552374543
Cami Baraian <74261431+Cami-Br@users.noreply.github.com>
2024-07-13 20:27:27 +0000

    add a touch of style

--- meeting_planner/meeting_planner/settings.py
+++ meeting_planner/meeting_planner/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,3 +136,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
